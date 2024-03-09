72313ab0245d4542f4a18598083accd46d1d5ad4
Tejas Gudi <101650493+tejas-gudi@users.noreply.github.com>
2024-03-09 22:01:20 +0530

    Create file2.cpp

---
+++ main/file2.cpp
@@ -0,0 +1,10 @@
+//File: file2.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "File2\n";
+  cout << "Hi, this is Tejas Gudi\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
